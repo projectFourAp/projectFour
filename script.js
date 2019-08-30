fc3e567428b592839c671eddcacc979ac9dcec50
Aaron <aaronjunehee@gmail.com>
2019-08-30 16:37:52 -0400

    added two new user parameters

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const teaApp = {} ;
 teaApp.baseURL = `https://developers.zomato.com/api/v2.1/search`;
 teaApp.apiKey = `ab555e02f8cc7344c070a66a218852f7`;
 
-teaApp.getTea = function (cuisineId) {
+teaApp.getTea = function (cuisineId, number, type) {
     $.ajax({
         url: `https://developers.zomato.com/api/v2.1/search`,
         headers: {
@@ -17,12 +17,12 @@ teaApp.getTea = function (cuisineId) {
             lat: 43.6483,
             lon: -79.3979,
             cuisines: cuisineId,
-            count: 5,
-            sort: 'real_distance',         
+            count: number,
+            sort: type,         
         }
     }).then( function (result) {
         console.log(result);
-        // teaApp.getRestaurant(result);
+        teaApp.getRestaurant(result);
     })
     
 }
@@ -32,36 +32,45 @@ teaApp.getTea = function (cuisineId) {
 
 teaApp.getRestaurant = function(result){
     console.log(result.restaurants);
+
     result.restaurants.forEach(function(cafe){
         console.log('bogo:', cafe.restaurant.include_bogo_offers);
         console.log('name:', cafe.restaurant.name);
         console.log('rating:', cafe.restaurant.user_rating.rating_text);
         console.log('aggregate-rating', cafe.restaurant.user_rating.aggregate_rating);
         console.log('cost-for-two:', cafe.restaurant.average_cost_for_two);
-        
-    })
 
+        if (cafe.restaurant.thumb) {
+            const foodHtml = `
+            <p>Name: ${cafe.restaurant.name}</p>
+            <p>Rating: ${cafe.restaurant.user_rating.aggregate_rating}</p>
+            <p>Cost for two: ${cafe.restaurant.average_cost_for_two}</p>
+            <p>Address: ${cafe.restaurant.location.address}</p>
+            <img src =${cafe.restaurant.thumb}>`
+                $('.container').append(foodHtml);
+        }
+    
+    
+    })
 
 }
 
 
 $(document).ready(function(){
-    // for(let i =0; i < 100; i=i+20){
-    //     teaApp.getTea(i);
-    // }
-    // teaApp.getTea();
-    // teaApp.getTea(73);
-    // teaApp.getTea(411);
-    // teaApp.getTea(320);
 
 
 
     $('form').on('submit', function(e){
         e.preventDefault();
-        const foodChoice = $(`input[name=food]:checked`).val();
-        console.log(foodChoice);
 
-        teaApp.getTea(foodChoice);
+        $('.container').empty();
+        const foodChoice = $(`input[name=food]:checked`).val();
+        const countChoice = $(`input[name=count]:checked`).val();
+        const sortChoice = $(`input[name=sort]:checked`).val();
+        console.log(foodChoice);
+        console.log(countChoice);
+
+        teaApp.getTea(foodChoice, countChoice, sortChoice);
 
 
     })
